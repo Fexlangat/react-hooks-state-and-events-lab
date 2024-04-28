917d7a584871253159af1188686fc569706cddab
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-04-28 14:45:35 +0300

    shopping list

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State to track whether the item is in the cart or not
+  const [inCart, setInCart] = useState(false);
+
+  // Function to toggle the state of inCart
+  const toggleCart = () => {
+    setInCart((prevInCart) => !prevInCart);
+  };
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      {/* Change button text based on whether the item is in the cart or not */}
+      <button className="add" onClick={toggleCart}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
